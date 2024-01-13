e03a059523b0e8533ffecc4574d89472c3f86d02
TomBroeske <tom.broeske@proton.me>
2024-01-13 18:51:46 +0100

    Initiated Project 5 + first steps

---
+++ Prototype 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 40;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    public Vector3 lookDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.name == "Player")
+        {
+        }
+
+
+    }
+
+}
